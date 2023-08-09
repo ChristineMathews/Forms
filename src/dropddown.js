59dacf1098d4e608ef13aa0679fdd9a77ad67485
ChristineMathews <christinemathews1998@gmail.com>
2023-08-09 13:20:06 +0530

    10 commit

--- src/dropddown.js
+++ src/dropddown.js
@@ -1,42 +1,25 @@
 import React from 'react';
-
-const styles = {
-    container: {
-        marginTop: '20px',
-        width: '200px',
-        position: 'relative',
-    },
-    select: {
-        width: '100%',
-        padding: '10px',
-        fontSize: '16px',
-        borderRadius: '5px',
-        border: '1px solid #ccc',
-        appearance: 'none',
-        backgroundColor: '#f4f4f4',
-    },
-    arrow: {
-        position: 'absolute',
-        top: '50%',
-        right: '10px',
-        transform: 'translateY(-50%)',
-        pointerEvents: 'none',
-    },
-};
+import './TeamDropdown.css'; // Import the CSS file for styling
 
 const TeamDropdown = ({ teams, onSelect }) => (
-    <div style={styles.container}>
-        <select style={styles.select} onChange={(e) => onSelect(e.target.value)}>
-            <option value="">Select a team...</option>
-            {teams.map((team) => (
-                <option key={team} value={team}>
-                    {team}
-                </option>
-            ))}
-        </select>
-        <div style={styles.arrow}>▼</div>
+    <div className="dropdown-container">
+        <div className="dropdown-card">
+            <h2>Select a Team</h2>
+            <div className="select-container">
+                <select onChange={(e) => onSelect(e.target.value)}>
+                    <option value="">Select a team...</option>
+                    {teams.map((team) => (
+                        <option key={team} value={team}>
+                            {team}
+                        </option>
+                    ))}
+                </select>
+                <div className="arrow">▼</div>
+            </div>
+        </div>
     </div>
 );
 
 export default TeamDropdown;
+
 
